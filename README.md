d081bacad8872f3fbd6d4468df614a6bb9a34620
andymaichez <andymaichez@gmail.com>
2024-09-15 08:53:18 +0300

    new commits

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Healthcare
-This is a hospital website.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//nav-bar
 document.getElementById('menubar').addEventListener('click', function() {
     document.getElementById('nav-bar').classList.toggle('active');
 });
